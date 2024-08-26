f4e08a2f3b628d53549c88b9c44af5d5de47b4c0
adamokennie <adamokennie@gmail.com>
2024-08-26 10:27:46 +0100

    insertdata

--- project/app/urls.py
+++ project/app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from app import views
 urlpatterns = [
     path('',views.index, name="index"),
-    path('about',views.about, name="about")
+    path('about',views.about, name="about"),
+    path('insert',views.insertData, name="insertData")
 ]
